8efc818f9091039620a03d7174bd1cc1bca46ae1
Andressa Valengo <valengoandressa@gmail.com>
2019-05-01 11:36:55 -0300

    fixed keyboard hiding text and buttons

--- back2present/Controller/ViewController.swift
+++ back2present/Controller/ViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var nameTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification, object: nil)
+        nameTextField.delegate = self
     }
     
     @IBAction func changeText(_ sender: Any) {
@@ -21,6 +26,27 @@ class ViewController: UIViewController {
             return
         }
         SceneRepository.name = name
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else {return}
+        guard let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {return}
+        let keyboardFrame = keyboardSize.cgRectValue
+        if self.view.frame.origin.y == 0 {
+            self.view.frame.origin.y -= keyboardFrame.height
+        }
+
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    func textFieldShouldReturn(_ nameTextField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
     }
 }
 
